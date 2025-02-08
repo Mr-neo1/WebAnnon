85d0e0d51c476ee806ec9a4d41bdae21a05c02a9
Mr_neo <90716969+Mr-neo1@users.noreply.github.com>
2025-02-08 12:28:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
